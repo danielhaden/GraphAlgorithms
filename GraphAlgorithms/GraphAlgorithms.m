a0e14fc28c610981abc1d6c07d0ec6936e706e90
Daniel Haden <haden.daniel@gmail.com>
2018-05-20 16:58:02 -0600

    First Commit

---
+++ GraphAlgorithms/GraphAlgorithms.m
@@ -0,0 +1,16 @@
+(* Wolfram Language Package *)
+
+(* Created by the Wolfram Workbench May 20, 2018 *)
+
+BeginPackage["GraphAlgorithms`"]
+(* Exported symbols added here with SymbolName::usage *) 
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+
+
+End[]
+
+EndPackage[]
+
---
+++ PacletInfo.m
@@ -0,0 +1,11 @@
+(* Paclet Info File *)
+
+(* created 2018.05.20*)
+
+Paclet[
+  Name -> "GraphAlgorithms",
+  Version -> "0.0.1",
+  MathematicaVersion -> "6+",
+  Extensions -> {
+    {"Documentation", Language -> "English"}
+}]
