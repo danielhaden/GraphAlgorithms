e52a317664810f9238daa2677973a6785d65da01
hadend <hadend@hadend-XPS-13-9370>
2018-05-21 22:04:57 -0600

    implemented nested triangle graph

--- GraphAlgorithms/GraphAlgorithms.m
+++ GraphAlgorithms/GraphAlgorithms.m
@@ -7,10 +7,17 @@ BeginPackage["GraphAlgorithms`"]
 
 Begin["`Private`"]
 (* Implementation of the package *)
-
-
-
 End[]
 
+NestedTriangleGraph[n_] :=
+  Graph[Flatten[Join[{{1 <-> 2}}, 
+  					 Outer[{#1 <-> #2} &, {1, 2}, Range[3, n + 2]], 
+     				 MapThread[{#1 <-> #2} &, {Range[3, n + 1], Range[4, n + 2]}]]],
+   
+   		VertexCoordinates -> Join[{{0., 0.}, {1., 0.}}, 
+     						      Transpose[{ConstantArray[0.5, n], 
+     						      			N[Rest[Subdivide[n]]]}]]
+  ];
+				
 EndPackage[]
 
